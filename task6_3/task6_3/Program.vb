aa8b8c840d30e1ba0422efc9b0b4f86b263e60b2
kongboming <48128050+kongboming@users.noreply.github.com>
2019-09-01 22:25:44 +0800

    task6.3
    这次的函数都是从1开始计数的
    mid函数的第二个参数是从字符串的哪里开始，第三个是要数几个字符
    
    asc就是单纯地将字符转乘ascii码，如果参数是字符串的化就默认转换字符串第一位

---
+++ task6_3/task6_3/Program.vb
@@ -0,0 +1,28 @@
+﻿'
+' Created by SharpDevelop.
+' User: ASUS
+' Date: 2019/9/1
+' Time: 22:04
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Module Program
+	Sub Main()
+		'表格的答案：
+		'|char.isnumber(string1,3)	|	false
+		'|asc('h')					|	104
+		'|char.isupper(password,6)	|	true
+		'|asc(mid(string1,5,1))		|	122
+		'|asc...>64 and asc...<92	|	true
+		Dim string1 As String ="Allez Allez!"',password As String="WinInMadrid"
+		Console.WriteLine(Char.IsLower(string1,3))
+		Console.WriteLine(Asc(Mid(string1,5,1)))
+		'Console.WriteLine(Asc("h"))
+		'If Asc(Mid(password,4,1))>64 And Asc(Mid(password,4,1))<92 Then
+		'	Console.WriteLine("true")
+		'Else
+		'	Console.WriteLine("false")
+		'End If
+		Console.ReadKey(True)
+	End Sub
+End Module
