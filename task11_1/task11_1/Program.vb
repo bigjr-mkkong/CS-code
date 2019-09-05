6b99a9bce6cf62d30edb1521467088c0c7ffc454
kongboming <48128050+kongboming@users.noreply.github.com>
2019-09-05 22:43:58 +0800

    task 11.1
    e~a~s~y~

---
+++ task11_1/task11_1/Program.vb
@@ -0,0 +1,20 @@
+﻿Module Program
+	Sub Main()
+		Dim input As String, maxstr As String, maxlen As Integer =-1
+		Console.WriteLine("please enter your strings and press enter, when you wish to quit, just press the enter key")
+		input=Console.ReadLine()
+		While input.Length>0
+			input=Console.ReadLine()
+			If input.Length>=maxlen Then
+				maxlen=input.Length
+				maxstr=input
+			End If
+		End While
+		If maxlen>-1 Then
+			Console.WriteLine("the longest string was " & maxstr & " with a length of " & maxlen)
+		Else
+			Console.WriteLine("you didn't enter anything...")
+		End If
+		Console.ReadKey(True)
+	End Sub
+End Module
