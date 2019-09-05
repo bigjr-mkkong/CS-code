dc30ea12b42c7748f4041d9c20b411181c270ebd
kongboming <48128050+kongboming@users.noreply.github.com>
2019-09-05 22:43:37 +0800

    task 10.2
    E~a~s~y~

---
+++ task10_2/task10_2/Program.vb
@@ -0,0 +1,12 @@
+﻿Module Program
+	Sub Main()
+		Dim input As String="default",cnt As Integer=0
+		Console.WriteLine("please enter your strings and press enter, when you wish to quit, just press the enter key")
+		While input.Length>0
+			input=Console.ReadLine()
+			cnt+=input.Length
+		End While
+		Console.WriteLine("you entered " & cnt & " character(s)")
+		Console.ReadKey(True)
+	End Sub
+End Module
