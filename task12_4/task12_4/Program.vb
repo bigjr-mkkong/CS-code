2e4ccb206723f874754424ea97223cba3e76b126
kongboming <48128050+kongboming@users.noreply.github.com>
2019-09-11 16:24:13 +0800

    task12,13
    also with some change in previous subject

---
+++ task12_4/task12_4/Program.vb
@@ -0,0 +1,36 @@
+﻿Module Program
+	Sub Main()
+		Dim in1 As Integer
+		Console.WriteLine("Please select a topic from below to learn more about:")
+		Console.WriteLine("1. Varibles")
+		Console.WriteLine("2. IF Statement")
+		Console.WriteLine("3. CASE Statement")
+		Console.WriteLine("4. Count Controlled Loops")
+		Console.WriteLine("5. Pre-Condition Loops")
+		Console.WriteLine("6. Post-Condition Loops")
+		Console.WriteLine("99. Exit")
+		While 1<2
+		In1=Console.ReadLine()
+		Select Case in1
+                Case = 1
+                    Console.WriteLine("dim [var1] as [type]")
+                Case = 2
+                    Console.WriteLine("if [condition] then [statemrnt]" & vbCrLf & "else if [condition] then [statement]" & vbCrLf & "endif")
+                Case = 3
+                    Console.WriteLine("select case [var1]" & vbCrLf & "case=[condition] [statement]" & vbCrLf & "scase=[condition] [statement]" & vbCrLf & " end select")
+                Case = 4
+                    Console.WriteLine("for [define loop var] step[step length]" & vbCrLf & "next [loop var]")
+                Case = 5
+                    Console.WriteLine("while [condition] then [statement]" & vbCrLf & "end while")
+                Case = 6
+                'post-condition loops
+                Case =99
+                    Exit Sub
+                Case Else
+                    Console.WriteLine("no option supported, please enter twice time")
+		End Select
+
+        End While
+        Console.ReadKey(True)
+	End Sub
+End Module
---
+++ task13_1/task13_1/Program.vb
@@ -0,0 +1,36 @@
+﻿Module Program
+	Sub Main()
+		'--------------------------------13.1.1
+		Dim rnd As New Random
+		For i=10  To 50 Step 1
+			Console.WriteLine(rnd.Next(10,50))
+		Next i
+		'--------------------------------13.1.2
+		Dim upbound As Integer,lowbound As Integer
+		upbound=Console.Readline()
+		lowbound=Console.ReadLine()
+		Dim ans As Integer=rnd.Next(lowbound,upbound),inp As Integer
+		inp=Console.ReadLine()
+		While inp>ans Or inp<ans
+			If inp<ans Then
+				Console.WriteLine("your input is too low")
+			Else
+				Console.WriteLine("your input is too high")
+			End If
+			inp=Console.ReadLine()
+		End While
+		Console.WriteLine("correct")
+		'----------------------a program to guess the answer--------------
+		'--------------------use binary research to guess the number------
+		Dim mid As Integer=(upbound-lowbound)/2
+		While mid<ans Or mid>ans
+			If mid<ans Then
+				mid=(mid+upbound)/2
+			Else
+				mid=(mid+lowbound)/2
+			End If
+		End While
+		Console.WriteLine(mid)
+        Console.ReadKey(True)
+	End Sub
+End Module
