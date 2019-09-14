d2c2561a981f6aad044b169697549e1b73c96fff
kongboming <48128050+kongboming@users.noreply.github.com>
2019-09-14 22:55:27 +0800

    task14.3

---
+++ task14_3/task14_3/Program.vb
@@ -0,0 +1,14 @@
+﻿Module Program
+	Sub Main()
+		Dim n As Integer, arr(100) As Integer, inp As Integer
+		console.WriteLine("how many elements are in your program? ")
+		n=Console.Readline()
+		For i As Integer = 1 To n
+			Console.WriteLine("please enter element " & i)
+			inp=Console.ReadLine()
+			arr(i)=inp
+		Next i
+				
+		Console.ReadKey(True)
+	End Sub
+End Module
