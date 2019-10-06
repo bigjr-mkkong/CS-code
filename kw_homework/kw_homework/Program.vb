ee93d44eb9ee5357c80f09e013eb4f4741636373
kongboming <48128050+kongboming@users.noreply.github.com>
2019-10-07 00:09:29 +0800

    kw国庆作业
    强制使用case语句的鲸鱼王是屑(确幸)

---
+++ kw_homework/kw_homework/Program.vb
@@ -0,0 +1,25 @@
+﻿Module Program
+	Sub Main()
+		Dim score As Integer
+		score=Console.ReadLine()
+		Select Case score
+			Case <=40
+				Console.Write("F" & vbCrLf & "Failed")
+		    Case <=50 
+		    	Console.Write("E" & vbCrLf & "Failed")
+		    Case <=60
+		    	Console.Write("D" & vbCrLf & "Miss")
+		    Case <=70
+		    	Console.Write("C" & vbCrLf & "Bad")
+		    Case <=80
+		    	Console.Write("B" & vbCrLf & "Good")
+		    Case <=90
+		    	Console.Write("A" & vbCrLf & "Great")
+		    Case <=100
+		    	Console.Write("A*" & vbCrLf & "Perfect")
+		    Case Else
+		    	Console.WriteLine("Score should in [0,100]")
+		End Select
+		Console.ReadKey(True)
+	End Sub
+End Module
