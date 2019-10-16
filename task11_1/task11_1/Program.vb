c922d1b93634dbd7714f826e3f72809eeed8e30e
kongboming <48128050+kongboming@users.noreply.github.com>
2019-10-16 10:54:28 +0800

    fix a bug
    一个小bug的修复

--- task11_1/task11_1/Program.vb
+++ task11_1/task11_1/Program.vb
@@ -4,11 +4,11 @@
 		Console.WriteLine("please enter your strings and press enter, when you wish to quit, just press the enter key")
 		input=Console.ReadLine()
 		While input.Length>0
-			input=Console.ReadLine()
 			If input.Length>=maxlen Then
 				maxlen=input.Length
 				maxstr=input
 			End If
+			input=Console.ReadLine()
 		End While
 		If maxlen>-1 Then
 			Console.WriteLine("the longest string was " & maxstr & " with a length of " & maxlen)
