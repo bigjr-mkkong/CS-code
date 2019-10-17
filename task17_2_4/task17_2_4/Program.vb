7a20d1a8fa3c93e17526cc618d8336acb69a45ce
kongboming <48128050+kongboming@users.noreply.github.com>
2019-10-17 12:42:58 +0800

    task 17.2.4
    没什么好说的

---
+++ task17_2_4/task17_2_4/Program.vb
@@ -0,0 +1,13 @@
+﻿Module Program
+	Sub Main()
+		Dim filewriter As IO.StreamWriter,input As String
+		filewriter=New IO.StreamWriter("test.txt",True)
+		input=Console.ReadLine()
+		While input <> "###"
+			filewriter.WriteLine(input)
+			input=Console.ReadLine()
+		End While
+		filewriter.Close()
+		Console.ReadKey(True)
+	End Sub
+End Module
