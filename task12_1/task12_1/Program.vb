155277d247e5091393dfde3d385be8c0c7363e37
kongboming <48128050+kongboming@users.noreply.github.com>
2019-10-23 09:59:40 +0800

    add some previous work

---
+++ task12_1/task12_1/Program.vb
@@ -0,0 +1,11 @@
+﻿Module Program
+	Sub Main()
+		Dim hour As Double,rate As Double
+		Console.WriteLine("please enter the hour(s) work: ")
+		hour=Console.ReadLine()
+		Console.WriteLine("please enter the work pay rate :")
+		rate=Console.ReadLine()
+		Console.WriteLine("total work pay is: " & hour*rate)
+		Console.ReadKey(True)
+	End Sub
+End Module
---
+++ task12_2/task12_2/Program.vb
@@ -0,0 +1,15 @@
+﻿Module Program
+	Sub Main()
+		Dim n As String
+		n=Console.ReadLine()
+		While n <> ""
+			For i As Integer = 1 To Math.Sqrt(Int(n))+1
+				If n Mod i =0 Then
+					Console.WriteLine(i & " " & n/i)
+				End If
+			Next i
+			n=Console.ReadLine()
+		End While
+		Console.ReadKey(True)
+	End Sub
+End Module
